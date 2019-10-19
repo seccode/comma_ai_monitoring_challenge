c57d456df7f78ee7d2806255b7c75794dcebd550
sam <clymersam@gmail.com>
2019-10-18 20:10:55 -0400

    added documentation

--- main.py
+++ main.py
@@ -3,16 +3,48 @@ import dlib
 import numpy as np
 from imutils import face_utils
 import argparse
-import time
 from sklearn.svm import SVC
 import glob
-import math
+
+'''
+Idea:
+    Use Dlib's facial landmark detection model to extract facial features,
+    and train a Support Vector Machine to Classify driving instances as either
+    'Attentive' or 'Inattentive'. The feature vector for the SVM is a 30,4 matrix
+    where the features are Eye:Face Area Ratio and the 3 Euler Angles of the
+    Head Rotation for the past 30 frames. Every frame is classifed by the SVM,
+    and a heuristic is set as 10 consecutive frames of inattentive classifications
+    being the definition of inattentiveness.
+
+Labeling:
+    Labels for the SVM are either Attentive or Inattentive (0 or 1). When the
+    --label flag is passed as an argument, holding the '0' and '1' will label
+    the feature vector representing the current frame as attentive and inattentive
+    respectively. Labeling different videos or the same video over and over will
+    not overwrite the feature/label pairs generated, but will add more data to
+    the .npy files in data/. When a video is called with the --predict flag, all
+    the feature/label pairs in the data file will be loaded and used to train
+    the SVM before predicting frames of the video.
+'''
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Pass video file and labeling/predction information")
+    parser.add_argument("--video",dest="video",default="0",
+                        help="Path to video file")
+    parser.add_argument("--label",dest="label",default=0,
+                        help="1 to label video data, labeling is done by \
+                        pressing the '1' key while video is played (or in real \
+                        time webcam) for inattentive; the default class is \
+                        attentive if no key is pressed for a frame")
+    parser.add_argument("--predict",dest="predict",default=0,
+                        help="1 to train SVM on given data and predict frames")
+    return parser.parse_args()
+
 
 class FaceDetector:
     def __init__(self):
         self.dist_coeffs = np.zeros((4,1))
         self.face_landmark_path = 'model/shape_predictor_68_face_landmarks.dat'
-        self.eyes_area = 0 # Store area of eye feature
         self.head_rotation = np.zeros((3,)) # (3,) Store head rotation information
 
     def get_model_points(self,filename='model/model.txt'):
@@ -25,7 +57,7 @@ class FaceDetector:
             model_points[:, 2] *= -1
             return model_points
 
-    def draw_annotation_box(self, frame,
+    def annotate_face(self, frame,
                                 shape,
                                 rotation_vector,
                                 translation_vector,
@@ -33,40 +65,28 @@ class FaceDetector:
                                 line_width=2):
             # Draw 3D box on frame in front of head
             point_3d = []
-            rear_size = 75
-            rear_depth = 0
-            point_3d.append((-rear_size, -rear_size, rear_depth))
-            point_3d.append((-rear_size, rear_size, rear_depth))
-            point_3d.append((rear_size, rear_size, rear_depth))
-            point_3d.append((rear_size, -rear_size, rear_depth))
-            point_3d.append((-rear_size, -rear_size, rear_depth))
-            front_size_side = 120
-            front_size_top = 120
+            rear = 75
+            front_side = 120
+            front_top = 120
             front_depth = 100
-            point_3d.append((-front_size_side, -front_size_top, front_depth))
-            point_3d.append((-front_size_side, front_size_top, front_depth))
-            point_3d.append((front_size_side, front_size_top, front_depth))
-            point_3d.append((front_size_side, -front_size_top, front_depth))
-            point_3d.append((-front_size_side, -front_size_top, front_depth))
+            for size, depth in zip([(rear,rear),(front_side,front_top)],[0,front_depth]):
+                for mults in [(-1,-1),(-1,1),(1,1),(1,-1),(-1,-1)]:
+                    point_3d.append((size[0]*mults[0],size[1]*mults[1],depth))
             point_3d = np.array(point_3d, dtype=np.float).reshape(-1, 3)
 
             # Project 3D points onto 2D frame
-            (point_2d, _) = cv2.projectPoints(point_3d,
-                                              rotation_vector,
-                                              translation_vector,
-                                              self.camera_matrix,
-                                              self.dist_coeffs)
+            point_2d = np.int32(cv2.projectPoints(point_3d,
+                                          rotation_vector,
+                                          translation_vector,
+                                          self.camera_matrix,
+                                          self.dist_coeffs)[0].reshape(-1, 2))
 
-            point_2d = np.int32(point_2d.reshape(-1, 2))
-
-            head_turned = False
-            # If head is turned, color annotation box green
+            # If head is not facing straight ahead, color head box green
             if point_2d[0][0] < point_2d[5][0] or \
                 point_2d[0][1] < point_2d[5][1] or \
                 point_2d[2][0] > point_2d[7][0] or \
                 point_2d[2][1] > point_2d[7][1]:
                 color = (0,255,0)
-                head_turned = True
 
             # Add lines to frame
             cv2.polylines(frame, [point_2d], True, color, line_width, cv2.LINE_AA)
@@ -83,7 +103,6 @@ class FaceDetector:
                     cv2.circle(frame,tuple(item),1,(0,255,0),-1)
                 else:
                     cv2.circle(frame,tuple(item),1,(255,0,0),-1)
-            return head_turned
 
     def get_head_pose(self,shape):
         # Find rotational and translational vectors of head position
@@ -96,13 +115,11 @@ class FaceDetector:
         rvec_matrix = cv2.Rodrigues(rotation_vec)[0]
         proj_matrix = np.hstack((rvec_matrix, translation_vec))
         eulerAngles = cv2.decomposeProjectionMatrix(proj_matrix)[6]
-        # pitch_roll_yaw = [math.radians(_) for _ in eulerAngles]
 
         return (rotation_vec, translation_vec, eulerAngles)
 
     def eyes_area_per_face_area(self,shape):
-        # Heuristic to determine if eyes are open based on a continuously
-        # calculated average eye area controlled for head size (proxy for depth)
+        # Calculate average eye area and update running tracker
         eye_left = np.array([shape[36],shape[37],shape[38],
                             shape[39],shape[40],shape[41]])
         eye_right = np.array([shape[42],shape[43],shape[44],
@@ -113,7 +130,6 @@ class FaceDetector:
 
         left_area = self.shoelace_formula(eye_left) / face_area
         right_area = self.shoelace_formula(eye_right) / face_area
-        self.eyes_area = ((self.eyes_area * self.count) + (left_area+right_area)/2) / (self.count + 1)
         return (left_area+right_area)/2
 
     def shoelace_formula(self,points):
@@ -133,11 +149,10 @@ class FaceDetector:
             all_features = np.concatenate((all_features,np.load(f_file)))
             all_labels = np.concatenate((all_labels,np.load(l_file)))
 
-        clf = SVC(class_weight='balanced')
+        clf = SVC(class_weight='balanced',probability=True)
         clf.fit(all_features,all_labels)
         print('Training Error: {}'.format(clf.score(all_features,all_labels)))
         return clf
-
 
     def preprocess_frame(self,frame):
         # Crop frame
@@ -145,15 +160,14 @@ class FaceDetector:
 
         # Resize frame for faster detection
         frame = cv2.resize(frame,(int(frame.shape[1]/2),int(frame.shape[0]/2)))
-        frame_copy = frame.copy()
+        frame_orig = frame.copy()
 
         # Adjust contrast and brightness of frame
         # Get histogram for grey scale frame
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         hist = cv2.calcHist([gray],[0],None,[256],[0,256])
         # Get CDF of histogram
-        cdf = []
-        cdf.append(float(hist[0]))
+        cdf = [float(hist[0])]
         [cdf.append(cdf[-1] + float(hist[index])) for index in range(1, len(hist))]
 
         hist_percent = (cdf[-1]*0.125)
@@ -164,8 +178,7 @@ class FaceDetector:
         beta = -min_gray * alpha
         frame = cv2.convertScaleAbs(frame, alpha=alpha, beta=beta)
 
-        return frame, frame_copy
-
+        return frame, frame_orig #Return frame copy for original frame coloring
 
     def detect(self):
         FEATURES = []
@@ -173,11 +186,11 @@ class FaceDetector:
         # Read from video and detect head position
         if args.video == '0':
             args.video = 0
-        cap = cv2.VideoCapture(args.video)
-        assert cap.isOpened(), "Video {} not found".format(args.video)
+        video = cv2.VideoCapture(args.video)
+        assert video.isOpened(), "Video {} not found".format(args.video)
 
         # Find size of frame
-        _, test_frame = cap.read()
+        _, test_frame = video.read()
         size = test_frame.shape
         self.focal_length = size[1]
         self.camera_center = (size[1] / 2, size[0] / 2)
@@ -195,7 +208,7 @@ class FaceDetector:
         self.predictor = dlib.shape_predictor(self.face_landmark_path)
 
         if args.predict:
-            # Train SVM
+            # Train SVM instantiate classifier
             clf = self.train_svm()
 
         # Driver state stores classifications of attentive/inattentive for
@@ -203,27 +216,25 @@ class FaceDetector:
         self.driver_state = np.zeros((30,))
 
         self.count = 0
-        while cap.isOpened():
-            ret, frame = cap.read()
+        while video.isOpened():
+            ret, frame = video.read()
             if not ret:
                 break
 
-
-            frame, frame_copy = self.preprocess_frame(frame) # Adjust frame to make detection easier
+            # Adjust frame to make detection easier
+            frame, frame_orig = self.preprocess_frame(frame)
 
             # Feature vector that represents driver state
-            # [Eye Area, Trans Vec X, Trans Vec Y, Trans Vec Z]
+            # [Eye to Face Area Ratio, Angle 1, Angle 2, Angle 3]
             current_state = np.array([[0,0,0,0]])
 
             # Detect face in frame
-            face_rects = self.detector(frame, 0)
+            faces = self.detector(frame, 0)
 
-            head_turned = True
-            eyes_area = 0
-            if len(face_rects) > 0: # Head detected
+            if len(faces) > 0: # Face detected
 
                 # Get head feature points from first detected head
-                shape = face_utils.shape_to_np(self.predictor(frame_copy, face_rects[0]))
+                shape = face_utils.shape_to_np(self.predictor(frame_orig, faces[0]))
 
                 # Find position of head
                 rotation_vec, translation_vec, eulerAngles = self.get_head_pose(shape)
@@ -231,13 +242,12 @@ class FaceDetector:
                 self.count += 1
                 for x in range(3): # Euler Angles
                     self.head_rotation[x] += (eulerAngles[x] - self.head_rotation[x]) / self.count
-                # print(self.head_rotation)
 
-                # Check if eyes are open
+                # Get average area of right and left eye
                 eyes_area = self.eyes_area_per_face_area(shape)
 
                 # Draw facial features
-                head_turned = self.draw_annotation_box(frame_copy,shape,rotation_vec,translation_vec)
+                self.annotate_face(frame_orig,shape,rotation_vec,translation_vec)
 
                 # Create translational vector adjusted for rolling average
                 controlled_translational_vec = eulerAngles.flatten() - self.head_rotation
@@ -246,39 +256,54 @@ class FaceDetector:
                 current_state = np.append(np.array([eyes_area]),
                                         controlled_translational_vec).reshape(1,4)
 
-            # print(self.driver_state.reshape(5,6))
-
             if args.label:
+                # Add state vector to features
                 FEATURES.append(current_state.flatten())
 
             # Use SVM to determine if inattentive
-            if args.predict or args.simple_predict:
-                if args.predict:
-                    # 1 if inattentive
-                    pred = clf.predict(current_state.reshape(1,-1))
-                else:
-                    pred = np.array([0])
-                    if head_turned or eyes_area < (self.eyes_area / 1.5):
-                        pred[0] = 1
-
+            if args.predict:
+                # 1 if inattentive
+                probs = clf.predict_proba(current_state.reshape(1,-1)).flatten()
+                pred = np.argmax(probs)
                 # Update driver state vector
                 self.driver_state = np.append(np.delete(self.driver_state,0),pred)
-                if np.min(self.driver_state[-20:]) == 1: # Last 10 frames are inattentive
-                    frame_copy = cv2.cvtColor(frame_copy, cv2.COLOR_BGR2GRAY)
 
-            cv2.imshow("Frame", frame_copy)
+                # Show SVM Classifier Bar
+                bar_length = frame.shape[1] - 30
+                red_length = int(bar_length*probs[1])
+                cv2.rectangle(frame_orig,(15,10),(15+red_length,40),
+                                (0,0,255),cv2.FILLED)
+                cv2.rectangle(frame_orig,(15+red_length,10),(bar_length,40),
+                                (0,255,0),cv2.FILLED)
+
+                if np.min(self.driver_state[-20:]) == 1: # If last 10 frames are inattentive
+                    # Put red border on frame
+                    frame_orig = cv2.copyMakeBorder(frame_orig, 5, 5, 5, 5,
+                                                    cv2.BORDER_CONSTANT, value=(0,0,255))
+
+                    # Put text on frame
+                    cv2.putText(frame_orig,"Inattentive: {}".format(round(100*probs[1],2)),(20,80),
+                                cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 255), 1)
+                else: # Attentive
+                    cv2.putText(frame_orig,"Inattentive: {}%".format(round(100*probs[1],2)),(20,80),
+                                cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1)
+                cv2.putText(frame_orig,"Attentive: {}%".format(round(100*probs[0],2)),(20,60),
+                            cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1)
+
+            cv2.imshow("Frame", frame_orig)
             key = cv2.waitKey(1)
+            # Use cv2 waitKey to label frame
             if args.label or key == 27:
                 if key == 27:
-                    LABELS.append(0)
+                    LABELS.append(0) #Attentive
                     break
-                elif np.max(current_state) == 0:
-                    LABELS.append(1)
-                elif key == ord('1'):
-                    LABELS.append(1)
+                elif key == ord('0'):
+                    LABELS.append(0) #Attentive
+                elif (key == ord('1') or \
+                        np.max(current_state) == 0): # Inattentive if not face detected
+                    LABELS.append(1) #Inattentive
                 else:
-                    LABELS.append(0)
-
+                    LABELS.append(0) #Attentive
 
         if args.label:
             # Save labeled instances to data file
@@ -288,20 +313,8 @@ class FaceDetector:
 
 
 if __name__ == '__main__':
-    # Parse arguments, default to webcam
-    parser = argparse.ArgumentParser(description="Pass video file")
-    parser.add_argument("--video",dest="video",default="0",
-                        help="Path to video file")
-    parser.add_argument("--label",dest="label",default=0,
-                        help="1 to label video data, labeling is done by \
-                        pressing the '1' key while video is played (or in real \
-                        time webcam) for inattentive, and the default class is \
-                        attentive if no key is pressed for a frame")
-    parser.add_argument("--predict",dest="predict",default=0,
-                        help="1 to train SVM on given data and predict frames")
-    parser.add_argument("--simple_predict",dest="simple_predict",default=0,
-                        help="1 to predict inattentiveness based on heuristic")
-    args = parser.parse_args()
+    # Parse arguments
+    args = parse_args()
 
     # Start detection
     detector = FaceDetector()
